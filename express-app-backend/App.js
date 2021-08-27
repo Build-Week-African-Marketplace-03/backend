ee16b7bd26268c91d463e194d563031648c7ecbd
beefybroccoli <tedkimemail@yahoo.com>
2021-08-26 19:38:39 -0700

    (no comment)

--- express-app-backend/App.js
+++ express-app-backend/App.js
@@ -15,7 +15,33 @@ const app = express();
 const PORT = 4000;
 const map_profile = new Map();
 
-// app.use(express.urlencoded({ extended: true }));
+/**
+ *
+ * @param {id, name, email, phone, address} req.body
+ */
+const helper_Create_Profile_And_Add_To_MapProfile = (req) => {
+  map_profile.set(req.body.id, {
+    id: req.body.id,
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    address: req.body.address,
+  });
+};
+
+const debug_show_Map_Profile_size = (input_text = "") => {
+  console.log(`${input_text}map_profile.size = ${map_profile.size}`);
+};
+
+const debug_show_request_path = (input_text = "") => {
+  console.log(`${input_text}req.path = ${req.path}`);
+};
+
+const debug_print_Map_Profile = (input_text = "") => {
+  console.log(`${input_text}map_profile = `, map_profile);
+};
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.raw());
 
@@ -34,29 +60,17 @@ app
   .route("/signup")
   //post
   .post((req, res) => {
-    // console.log(
-    //   "map_profile.has(req.params.id) === false return ",
-    //   map_profile.has(req.params.id) === false
-    // );
-    console.log("req.body = ", req.body);
-    // console.log("req.body.name = ", req.body.params.name);
+    if (map_profile.has(req.body.id) === false) {
+      //add the new profile to Map_Profile
+      helper_Create_Profile_And_Add_To_MapProfile(req);
 
-    if (true) {
-      // if (map_profile.has(req.params.id) === false) {
-      // const { id, name, email, phone, address } = req.params.data;
-      // map_profile.set(req.params.id, {
-      // id: req.body["id"],
-      // name: req.body.name,
-      // email: req.body.email,
-      // phone: req.body.phone,
-      // address: req.body.address,
-      // });
-      console.log("map_profile.size = ", map_profile.size);
-      console.log("map_profile = ", map_profile);
+      debug_print_Map_Profile("/signup post - ");
+
       res.send({ status: "200", message: "Successfully posted profile" });
     } //end if
     else {
-      console.log("map_profile.size = ", map_profile.size);
+      // console.log("map_profile.size = ", map_profile.size);
+      console.log("Failed to posted profile");
       res.send({ status: "300", message: "Failed to posted profile" });
     }
   });
@@ -65,21 +79,22 @@ app
   .route("/profile/:id")
   //get
   .get((req, res) => {
-    const { id } = req.params;
-    console.log("req.params.route = ", req.params.route);
-    console.log("map_profile.size = ", map_profile.size);
-    console.log(map_profile);
-    console.log("id = ", id);
-    console.log("map_profile.has(id) = ", map_profile.has(id));
-    if (map_profile.has(id)) {
-      res.json(map_profile.get(id));
+    if (map_profile.has(req.params.id)) {
+      res.json(map_profile.get(req.params.id));
     } else {
       res.send({ status: "300", message: "Failed to retrieve profile" });
     }
   })
   //put
   .put((req, res) => {
-    res.send(`put request sucessfully on port ${PORT}`);
+    if (map_profile.has(req.body.id)) {
+      map_profile.delete(req.body.id);
+      helper_Create_Profile_And_Add_To_MapProfile(req);
+      debug_print_Map_Profile("/profile/:id put - ");
+      res.send({ status: "200", message: "Successfully update profile" });
+    } else {
+      res.send({ status: "300", message: "Failed to update profile" });
+    }
   });
 
 app
@@ -120,11 +135,7 @@ app.use((error, request, response, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log("express-app-backend at port ", PORT);
+  console.log("============================================");
+  console.log("start express-app-backend at port ", PORT);
   //   console.log(data);
 });
-
-/**
- *
- *
- */
